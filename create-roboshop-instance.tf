165bfe1e55d641d8b44bef2267dd1729ffa179eb
Juhi K <jkesarwani.us@gmail.com>
2023-04-26 15:12:46 -0400

    cart

--- create-roboshop-instance.tf
+++ create-roboshop-instance.tf
@@ -1,4 +1,22 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "catalogue" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
