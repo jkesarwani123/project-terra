67fa5931131a4bf674dec9e65a87b3f582b88d55
Juhi K <jkesarwani.us@gmail.com>
2023-04-26 22:48:48 -0400

    automate instance creation

--- create-roboshop-instance.tf
+++ create-roboshop-instance.tf
@@ -16,60 +16,76 @@ output "ami" {
 value=data.aws_ami.centos.image_id
 }
 
-resource "aws_instance" "frontend" {
+variable "components" {
+  default = [ "frontend","mongodb","catalogue" ]
+}
+
+resource "aws_instance" "instance" {
+  count         = length ( var.components )
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instanceTy
   vpc_security_group_ids = [ data.aws_security_group.selected.id ]
 
   tags = {
-    Name = "frontend"
+    Name = var.components[count.index]
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z0598390HIMZ28WHPBM2"
-  name    = "frontend.mushitude.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-//output "frontend" {
-// value=aws_instance.frontend.private_ip
-//}
-
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instanceTy
-  vpc_security_group_ids = [ data.aws_security_group.selected.id ]
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z0598390HIMZ28WHPBM2"
-  name    = "mongodb.mushitude.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instanceTy
-  vpc_security_group_ids = [ data.aws_security_group.selected.id ]
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z0598390HIMZ28WHPBM2"
-  name    = "catalogue.mushitude.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
+#
+#resource "aws_instance" "frontend" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instanceTy
+#  vpc_security_group_ids = [ data.aws_security_group.selected.id ]
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#}
+#
+#resource "aws_route53_record" "frontend" {
+#  zone_id = "Z0598390HIMZ28WHPBM2"
+#  name    = "frontend.mushitude.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.frontend.private_ip]
+#}
+#
+#//output "frontend" {
+#// value=aws_instance.frontend.private_ip
+#//}
+#
+#resource "aws_instance" "mongodb" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instanceTy
+#  vpc_security_group_ids = [ data.aws_security_group.selected.id ]
+#
+#  tags = {
+#    Name = "mongodb"
+#  }
+#}
+#
+#resource "aws_route53_record" "mongodb" {
+#  zone_id = "Z0598390HIMZ28WHPBM2"
+#  name    = "mongodb.mushitude.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.mongodb.private_ip]
+#}
+#
+#resource "aws_instance" "catalogue" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instanceTy
+#  vpc_security_group_ids = [ data.aws_security_group.selected.id ]
+#
+#  tags = {
+#    Name = "catalogue"
+#  }
+#}
+#
+#resource "aws_route53_record" "catalogue" {
+#  zone_id = "Z0598390HIMZ28WHPBM2"
+#  name    = "catalogue.mushitude.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.catalogue.private_ip]
+#}
