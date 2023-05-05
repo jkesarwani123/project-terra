16ffeb41e700213893ac5564b666278ee01269be
Juhi K <jkesarwani.us@gmail.com>
2023-05-04 20:16:58 -0400

    terra loops

--- statefiles.tf
+++ statefiles.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jkterra"
-    key    = "project-terra/terraform.tfstate"
+    key    = "roboshop/dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
