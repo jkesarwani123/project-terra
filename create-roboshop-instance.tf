d71debf0eecb4a5a429b3235e052e3669dbcfa48
Juhi K <jkesarwani.us@gmail.com>
2023-05-07 20:40:10 -0400

    roboshop-proj

--- create-roboshop-instance.tf
+++ create-roboshop-instance.tf
@@ -21,6 +21,7 @@ resource "aws_route53_record" "records" {
 }
 
 resource "null_resource" "provisioner"{
+  depends_on = [aws_instance.instance,aws_route53_record.records]
   for_each = var.components
   provisioner "remote-exec" {
   connection {
