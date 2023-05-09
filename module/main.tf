89b1b4534efd42250b3da716dced826e4f30c8e4
Juhi K <jkesarwani.us@gmail.com>
2023-05-09 09:30:29 -0400

    roboshop-proj

--- module/main.tf
+++ module/main.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "records" {
   name    = "${var.component_name}.jkdevops.online"
   type    = "A"
   ttl     = 30
-  records = "aws_instance.instance.private_ip"
+  records = ["aws_instance.instance.private_ip"]
 }
 
 resource "null_resource" "provisioner"{
