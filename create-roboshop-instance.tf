f90153f0e36001691f12286fdb21b8aafcec8f57
Juhi K <jkesarwani.us@gmail.com>
2023-05-09 09:06:45 -0400

    roboshop-proj

--- create-roboshop-instance.tf
+++ create-roboshop-instance.tf
@@ -1,41 +1,8 @@
-
-resource "aws_instance" "instance" {
-  //count         = length ( var.components )
+module "servers"{
   for_each = var.components
-  ami           = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [ data.aws_security_group.selected.id ]
-
-  tags = {
-    Name = each.value["name"]
-  }
-}
-
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id = "Z08610883Q0OU1R5RFMYM"
-  name    = "${each.value["name"]}.jkdevops.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance[each.value["name"]].private_ip]
-}
-
-resource "null_resource" "provisioner"{
-  depends_on = [aws_instance.instance,aws_route53_record.records]
-  for_each = var.components
-  provisioner "remote-exec" {
-  connection {
-    type     = "ssh"
-    user     = "centos"
-    password = "DevOps321"
-    host     = aws_instance.instance[each.value["name"]].private_ip
-  }
-
-    inline = [
-      "rm -rf Sample-Project",
-      "git clone https://github.com/jkesarwani123/Sample-Project.git",
-      "cd Sample-Project",
-      "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password","null")}"
-      ]
-  }
+  source="./module"
+  component_name=each.value["name"]
+  env=var.env
+  instance_type=each.value["instance_type"]
+  password=lookup(each.value,"password","null")
 }
---
+++ module/main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ data.aws_security_group.selected.id ]
+
+  tags = {
+    Name = var.component_name
+  }
+}
+
+resource "aws_route53_record" "records" {
+  for_each = var.components
+  zone_id = "Z08610883Q0OU1R5RFMYM"
+  name    = "${var.component_name}.jkdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = aws_instance.instance.private_ip
+}
+
+resource "null_resource" "provisioner"{
+  depends_on = [aws_instance.instance,aws_route53_record.records]
+  for_each = var.components
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+
+    inline = [
+      "rm -rf Sample-Project",
+      "git clone https://github.com/jkesarwani123/Sample-Project.git",
+      "cd Sample-Project",
+      "sudo bash ${var.component_name.sh} ${var.password}"
+    ]
+  }
+}
