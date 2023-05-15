ae787fd1121e0ab02fd3fa119f0ec825d920c71c
Juhi K <jkesarwani.us@gmail.com>
2023-05-15 18:33:47 -0400

    terraform ansible

--- module/main.tf
+++ module/main.tf
@@ -70,20 +70,7 @@ resource "aws_iam_role_policy" "ssm_roboshop_policy" {
 
     "Version": "2012-10-17",
     "Statement": [
-  {
-    "Effect": "Allow",
-    "Action": [
-    "cloudwatch:PutMetricData",
-    "ds:CreateComputer",
-    "ds:DescribeDirectories",
-    "ec2:DescribeInstanceStatus",
-    "logs:*",
-    "ssm:*",
-    "ec2messages:*"
-  ],
-    "Resource": "*"
-  },
-  {
+    {
     "Effect": "Allow",
     "Action": "iam:CreateServiceLinkedRole",
     "Resource": "arn:aws:iam::*:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM*",
@@ -93,24 +80,64 @@ resource "aws_iam_role_policy" "ssm_roboshop_policy" {
   }
   }
   },
-  {
-    "Effect": "Allow",
-    "Action": [
-    "iam:DeleteServiceLinkedRole",
-    "iam:GetServiceLinkedRoleDeletionStatus"
-  ],
-    "Resource": "arn:aws:iam::*:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM*"
-  },
-  {
-    "Effect": "Allow",
-    "Action": [
-    "ssmmessages:CreateControlChannel",
-    "ssmmessages:CreateDataChannel",
-    "ssmmessages:OpenControlChannel",
-    "ssmmessages:OpenDataChannel"
-  ],
-    "Resource": "*"
-  }
-  ]
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "kms:GetParametersForImport",
+          "kms:Decrypt",
+          "kms:ListAliases"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:DescribeDocumentParameters",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "*"
+#        "Resource": [
+#          "arn:aws:ssm:us-east-1:043742147815:document/dev.frontend.*",
+#          "arn:aws:ssm:us-east-1:043742147815:parameter/dev.frontend.*"
+#        ]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "cloudwatch:PutMetricData",
+          "ds:CreateComputer",
+          "ds:DescribeDirectories",
+          "ec2:DescribeInstanceStatus",
+          "logs:*",
+          "ssm:*",
+          "ec2messages:*"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "iam:DeleteServiceLinkedRole",
+          "iam:GetServiceLinkedRoleDeletionStatus"
+        ],
+        "Resource": "arn:aws:iam::*:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel"
+        ],
+        "Resource": "*"
+      }
+
+    ]
   })
 }
